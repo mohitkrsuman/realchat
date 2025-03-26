257c472b43a94911d6a7324a4d7d9e358625d64e
mohitkrsuman <m123aryan123@gmail.com>
2025-03-26 10:12:28 +0530

    formating button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -42,9 +42,13 @@ const Button: FC<ButtonProps> = ({
   ...props
 }) => {
   return (
-    <button className={cn(buttonVariants({ variant, size, className }))} disabled={isLoading || props.disabled} {...props}>
-        {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin"/> : null}
-        {children}
+    <button
+      className={cn(buttonVariants({ variant, size, className }))}
+      disabled={isLoading || props.disabled}
+      {...props}
+    >
+      {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}
+      {children}
     </button>
   );
 };
